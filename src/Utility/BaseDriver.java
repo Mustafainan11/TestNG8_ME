da487f0315b72f02a5581406332d0c898a8c759e
mustafainan5858@gmail.com <mustafainan5858@gmail.com>
2025-04-07 20:26:01 +0300

    LogTutma.info(sonuc.getName()+",  Metod Bitti"+" Sonuc="+ (sonuc.getStatus()==1 ? "Passed" : "Failed") );
            LogTutma.warn("WARN : Metod bitti, hata oluşmuş olsa idi bu şekilde eklenebilir.Bir if kontrolü ile");

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -1,17 +1,23 @@
 package Utility;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
 
 public class BaseDriver {
+    public static Logger LogTutma = LogManager.getLogger();// Logları ekleyeceğim nesneyi başlattım.
 
     // aşağısını SDET8 den aldık
     public static WebDriver driver;//her driver tanımlasın diye başa koyduk
@@ -19,6 +25,8 @@ public class BaseDriver {
 
     @BeforeClass//
     public void Setup() {
+        LogTutma.info("Log Tutma işlemi başladı");
+        // seleniumdaki static yani başlangıç kısımlar konacak
         // seleniumdaki static yani başlangıç kısımlar konacak
         driver = new ChromeDriver();
 
@@ -27,11 +35,13 @@ public class BaseDriver {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // 5 sn mühlet: elementi bulma mühleti
 
         wait=new WebDriverWait(driver,Duration.ofSeconds(20));//ELEMENT ÇIKANA KADAR BEKLE
+        LogTutma.info("Başlangıç değişkenleri driver,wait,log tanımlandı ve başlatıldı");
         LoginTest();
     }
 
     public void LoginTest() {
         System.out.println("Login Test başladı");
+        LogTutma.info("Login Test başladı");
         driver.get("http://opencart.abstracta.us/index.php?route=account/login");
         MyFunc.Bekle(2);
 
@@ -50,6 +60,7 @@ public class BaseDriver {
         Assert.assertTrue(driver.getTitle().equals("My Account"), "Login olunamadı");
 
         System.out.println("Login Test bitti");
+        LogTutma.info("Login Testi tamamlandı");
     }
         @AfterClass
         public void TearDown ()
@@ -57,6 +68,25 @@ public class BaseDriver {
             // seleniumdaki BekleKapat
             MyFunc.Bekle(3);
             driver.quit();  // bütün açılmış windowları kapatır
+            LogTutma.info("Driver kapatıldı");
+
+            //if (hata oldu ise)
+            LogTutma.warn("Driver kapatılamadı.");
         }
+    @BeforeMethod
+    public void BeforeMetod()
+    {
+        LogTutma.info("Metod başladı");
+
+        LogTutma.warn("WARN : Metod başladı, hata oluşmuş olsa idi");
+    }
+    @AfterMethod
+    public void AfterMethod(ITestResult sonuc){//tesin sonuç ve isim bilgisini olduğu değişkeni
+        LogTutma.info(sonuc.getName()+",  Metod Bitti"+" Sonuc="+ (sonuc.getStatus()==1 ? "Passed" : "Failed") );
+
+        LogTutma.warn("WARN : Metod bitti, hata oluşmuş olsa idi bu şekilde eklenebilir.Bir if kontrolü ile");
+
+
+    }
 
     }
